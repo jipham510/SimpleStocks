48bfd6f18bcad537ab1ac1054aa7ef5738e01610
Jimmy Pham <jipham510@gmail.com>
2019-09-19 10:14:16 -0700

    refactored stock items to fetch intraday data instead of latest stock price

--- frontend/components/stock/stock_index_item.jsx
+++ frontend/components/stock/stock_index_item.jsx
@@ -8,11 +8,47 @@ class StockIndexItem extends React.Component {
     constructor(props) {
         super(props);
         this.renderShares = this.renderShares.bind(this);
+        this.state = {
+            intradayData: [],
+            price: 0
+        }
+        this.setData = this.setData.bind(this);
     }
     componentDidMount(){
-        this.props.fetchLatestStockPrice(this.props.stock.ticker)
+        if (this.props.intradayData) {
+            this.setData(this.props.intradayData)
+        } else {
+            this.props.fetchIntradayData(this.props.stock.ticker)
+        }
     }
+    componentDidUpdate(prevProps){
+        if(this.props.intradayData !== prevProps.intradayData){
+            this.setData(this.props.intradayData)
+        }
+    }
+    setData(chart){
 
+        // let firstValidPrice
+        // for (let i = 0; i < chart.length; i++) {
+        //     if (chart[i].close !== null) {
+        //         firstValidPrice = chart[i].close
+        //         break;
+        //     }
+        // }
+
+        let lastValidPrice
+        for (let i = chart.length - 1; i >= 0 ; i--) {
+            if (chart[i].close !== null) {
+                lastValidPrice = chart[i].close
+                break;
+            }
+        }
+        return this.setState({
+            intradayData: chart,
+            price: lastValidPrice
+        })        
+
+    }
     renderShares(){
         if (this.props.stock.shares) 
         {
@@ -36,7 +72,7 @@ class StockIndexItem extends React.Component {
                     {this.renderShares()}
                 </div>
                 <div className="stock-index-item-price">
-                    {parseFloatToDollars(this.props.price)}
+                    {parseFloatToDollars(this.state.price)}
                 </div>
             </div>
         </Link>
--- frontend/components/stock/stock_index_item_container.js
+++ frontend/components/stock/stock_index_item_container.js
@@ -1,20 +1,25 @@
 import { connect } from 'react-redux';
-import { fetchLatestStockPrice } from '../../actions/stock_actions';
+import { fetchLatestStockPrice, fetchIntradayData } from '../../actions/stock_actions';
 import StockIndexItem from './stock_index_item';
-import { withRouter } from 'react-router-dom';
 
 const mapStateToProps = (state, ownProps) => {
     let stock = state.entities.stocks[ownProps.stock.ticker];
-    let price
-    if (stock) price = stock.price;
+
+    let intradayData;
+
+    if(stock && stock.intradayData) {
+        intradayData = stock.intradayData
+    }
     return {
         stock: ownProps.stock,
-        price
+        intradayData
     }
 }
 
 const mapDispatchToProps = dispatch => ({
-    fetchLatestStockPrice: (ticker) => dispatch(fetchLatestStockPrice(ticker))
+    fetchLatestStockPrice: (ticker) => dispatch(fetchLatestStockPrice(ticker)),
+    fetchIntradayData: (ticker) => dispatch(fetchIntradayData(ticker)),
+
 });
 
 export default connect(
