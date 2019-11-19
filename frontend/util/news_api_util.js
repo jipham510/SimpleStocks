e4844dd304721414e20f755201940560e8c5fb02
Jimmy Pham <jipham510@gmail.com>
2019-11-19 10:31:02 -0800

    fix news fetch

--- frontend/util/news_api_util.js
+++ frontend/util/news_api_util.js
@@ -1,9 +1,18 @@
 const apiKey = "bdc2169d1a2c44c8902f98865fc6a33f";
 
+//old call, images broke
+// export const fetchNews = () => (
+//     $.ajax({
+//         method: "GET",
+//         url: `https://newsapi.org/v2/top-headlines?country=us&pageSize=20&category=business&apiKey=${apiKey}`
+//     })
+// )
+
+//specific call to cnn
 export const fetchNews = () => (
     $.ajax({
         method: "GET",
-        url: `https://newsapi.org/v2/top-headlines?country=us&pageSize=20&category=business&apiKey=${apiKey}`
+        url: `https://newsapi.org/v2/everything?language=en&q=stocks&sources=cnn&pageSize=20&apiKey=${apiKey}`
     })
 )
 export const fetchCompanyNews = (company) => (
